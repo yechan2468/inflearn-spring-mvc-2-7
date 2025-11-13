15dc5016360d777d9205e9c9f4d5201a8e50df18
yechan <yechan24680@gmail.com>
2025-11-13 21:32:18 +0900

    first commit

---
+++ src/main/java/yechan/inflearn_spring_mvc_2_7/InflearnSpringMvc27Application.java
@@ -0,0 +1,13 @@
+package yechan.inflearn_spring_mvc_2_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringMvc27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringMvc27Application.class, args);
+	}
+
+}
---
+++ src/test/java/yechan/inflearn_spring_mvc_2_7/InflearnSpringMvc27ApplicationTests.java
@@ -0,0 +1,13 @@
+package yechan.inflearn_spring_mvc_2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringMvc27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
